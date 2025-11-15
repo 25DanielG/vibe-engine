5781836e7dee53c40cb001d3680ce0f9f4a0f49f
Daniel Gergov <danalmaden24@gmail.com>
2025-11-15 02:32:24 -0500

    update gitignore

--- server/dist/utils/fillPrompt.js
+++
@@ -1,6 +0,0 @@
-export function renderTemplate(template, params) {
-    return template.replace(/{{\s*([\w.-]+)\s*}}/g, (_, key) => {
-        const value = params[key];
-        return value !== undefined && value !== null ? String(value) : "";
-    });
-}
